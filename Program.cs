6c73c4c6607ebc10f069a81836b2ad2e8208ffc5
nadjapadali <padzeja@gmail.com>
2023-02-17 19:18:43 +0100

    Commit Ignore

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿static int Acker(int n,int m)
+{
+    if (n==0)
+      return m+1;
+    else
+        if((n!=0)&&(m==0))
+          return Acker(n-1,1);
+          else
+            return Acker(n-1,Acker(n,m-1));
+}    
+Console.Write(Acker(2,5));
+
+
