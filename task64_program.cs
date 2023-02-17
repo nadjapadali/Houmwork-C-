5fee18b99e5d1f230bc76cd55c3c886fb5509308
nadjapadali <padzeja@gmail.com>
2023-02-17 22:12:59 +0100

    Commit tasks

---
+++ task64_program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Введите целое число");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{NumRec(N)}");
+
+
+string NumRec(int N)
+{
+    if (N == 0) return "";
+    return " " + N +  NumRec(N-1);
+}
+
---
+++ task66_program.cs
@@ -0,0 +1,6 @@
+﻿static int SumRec(int M, int N)
+{
+    if (M >= N) return N;
+    else return N + SumRec(M, N - 1);
+}
+Console.Write(SumRec(5,6));
